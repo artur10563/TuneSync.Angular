a39993eefe61fe7a8c0f64242123123aa860acdf
artur10563 <galkaartur2004galkaartur@gmail.com>
2024-11-07 18:46:44 +0200

    package update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,26 +7,19 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { YoutubeVideoComponent } from './components/youtube-video/youtube-video.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { SongTableComponent } from './components/song-table/song-table.component';
 import { PlayerComponent } from './components/player/player.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    SidebarComponent,
-    NavbarComponent,
-    YoutubeVideoComponent,
-    SongTableComponent,
-    PlayerComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        SidebarComponent,
+        NavbarComponent,
+        YoutubeVideoComponent,
+        SongTableComponent,
+        PlayerComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
