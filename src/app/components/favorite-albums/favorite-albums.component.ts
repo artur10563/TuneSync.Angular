422409be26fdfcb88685e95e9854b56d8e1f8cf5
artur10563 <galkaartur2004galkaartur@gmail.com>
2025-02-21 20:01:09 +0200

    Filter for albums/playlists; Mix from fav

--- src/app/components/favorite-albums/favorite-albums.component.ts
+++ src/app/components/favorite-albums/favorite-albums.component.ts
@@ -1,10 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { AlbumService } from '../../services/album.service';
 import { NotificationService } from '../../services/notification.service';
 import { AlbumSummary } from '../../models/Album/AlbumSummary.model';
 import { PlaylistSummary } from '../../models/Playlist/PlaylistSummary.mode';
 import { PlaylistService } from '../../services/playlist.service';
 import { ActivatedRoute } from '@angular/router';
+import { MixService } from '../../services/mix.service';
+import { debounceTime, distinctUntilChanged, Subject } from 'rxjs';
 
 @Component({
     selector: 'app-favorite-albums',
@@ -13,36 +15,67 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class FavoriteAlbumsComponent {
     albums: AlbumSummary[] | PlaylistSummary[] = [];
+    filteredAlbums: AlbumSummary[] | PlaylistSummary[] = [];
+    private filterSubject: Subject<string> = new Subject<string>();
+    filterText: string = "";
+    isAlbumView: boolean = false;
 
     constructor(
         private albumService: AlbumService,
         private playlistService: PlaylistService,
         private notificationService: NotificationService,
-        private route: ActivatedRoute) { }
+        private route: ActivatedRoute,
+        private mixService: MixService) { }
+
+
 
     ngOnInit(): void {
-        let isAlbumView = this.route.snapshot.data['isAlbumView'];
+        this.isAlbumView = this.route.snapshot.data['isAlbumView'];
+        const serviceCall = this.isAlbumView
+            ? this.albumService.getFavoriteAlbums()
+            : this.playlistService.getFavoritePlaylists();
 
-        if (isAlbumView) {
-            this.albumService.getFavoriteAlbums().subscribe({
-                next: (albums) => {
-                    this.albums = albums;
-                },
-                error: (err) => {
-                    this.notificationService.handleError(err);
-                }
-            });
-        }
-        else {
+        serviceCall.subscribe({
+            next: (result) => {
+                this.albums = result;
+                this.filteredAlbums = this.albums;
+            },
+            error: (err) => {
+                this.notificationService.handleError(err);
+            }
+        });
 
-            this.playlistService.getFavoritePlaylists().subscribe({
-                next: (playlists) => {
-                    this.albums = playlists;
-                },
-                error: (err) => {
-                    this.notificationService.handleError(err);
-                }
+        //Filter after 300ms typing pause
+        this.filterSubject
+            .pipe(debounceTime(300), distinctUntilChanged())
+            .subscribe(() => {
+                this.filterAlbums();
             });
-        }
+    }
+
+
+    filterAlbums() {
+        this.filteredAlbums = this.albums.filter((album: AlbumSummary | PlaylistSummary) => {
+            const matchesTitle = album.title.toLowerCase().includes(this.filterText.toLowerCase());
+            const matchesArtist =
+                (album as AlbumSummary).artist?.displayName.toLowerCase().includes(this.filterText.toLowerCase());
+
+            return matchesTitle || matchesArtist;
+        });
+    }
+
+    onFilterChange(): void {
+        this.filterSubject.next(this.filterText);
+    }
+
+    mixFiltered() {
+        this.filteredAlbums.forEach(album => {
+            this.mixService.addAlbumToSelection(album);
+        });
+        this.mixService.startMix();
+    }
+
+    trackByGuid(index: number, album: AlbumSummary | PlaylistSummary): string {
+        return album.guid;
     }
 }
