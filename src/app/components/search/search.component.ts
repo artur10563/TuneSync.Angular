b31d70abca413da411a4d188d1aec65291012992
artur10563 <galkaartur2004galkaartur@gmail.com>
2025-02-28 20:57:25 +0200

    artists in search

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -5,24 +5,25 @@ import { takeUntil } from 'rxjs/operators';
 import { Song } from '../../models/Song/Song.model';
 import { YoutubeSong } from '../../models/Youtube/YoutubeSong.model';
 import { SongService } from '../../services/song.service';
+import { Artist } from '../../models/Artist/Artist.model';
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrl: './search.component.css'
+    selector: 'app-search',
+    templateUrl: './search.component.html',
+    styleUrl: './search.component.css'
 })
 export class SearchComponent implements OnInit, OnDestroy {
     private unsubscribe$ = new Subject<void>();
-    
+
     youtubeSongs: YoutubeSong[] = [];
     songs: Song[] = [];
-    
+    artists: Artist[] = [];
     currentPage = 1;
     totalPages = 1;
     searchQuery = '';
     showYoutubeSearchButton = false;
 
-    constructor(private songService: SongService, private route: ActivatedRoute) {}
+    constructor(private songService: SongService, private route: ActivatedRoute) { }
 
     ngOnInit() {
         this.route.queryParams.pipe(takeUntil(this.unsubscribe$)).subscribe(params => {
@@ -31,7 +32,7 @@ export class SearchComponent implements OnInit, OnDestroy {
                 this.fetchSongs(true);
             }
         });
-    
+
         this.songService.youtubeSongs$.pipe(takeUntil(this.unsubscribe$)).subscribe((songs) => {
             this.youtubeSongs = songs;
         });
@@ -39,20 +40,24 @@ export class SearchComponent implements OnInit, OnDestroy {
 
     fetchSongs(isNewSearch = false) {
         if (isNewSearch) {
-            this.songs = []; 
+            this.songs = [];
             this.youtubeSongs = [];
+            this.artists = [];
             this.currentPage = 1;
             this.showYoutubeSearchButton = true;
         }
 
         this.songService.searchDbSongs(this.searchQuery, this.currentPage).subscribe(response => {
-            if(!response){
+            if (!response) {
                 this.fetchYoutubeSongs();
                 return;
             }
 
             this.songs = [...this.songs, ...response.items];
             this.totalPages = response.pageInfo.totalPages;
+            this.artists = [
+                ...new Map(this.songs.map(x => [x.artist.guid, x.artist])).values()
+              ];
 
         });
     }
